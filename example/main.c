820d110791650af4187d7889fb6ab8a04490361b
Amirhossein Askari <theamiraskarii@gmail.com>
2025-06-16 15:00:11 +0330

    Example Code

--- example/main.c
+++ example/main.c
@@ -1,216 +1,55 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
-#include "i2c.h"
-#include "gpio.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
 #include "i2c_memory.h"
 
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
 
 I2C_MemoryTypeDef memory1, memory2;
-I2C_Memory_StatusTypeDef status;
+I2C_Memory_StatusTypeDef status[8];
 
-uint8_t data;
 uint8_t data_list[5U];
+uint8_t data;
 
-/* USER CODE END PV */
 
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  /* USER CODE BEGIN 2 */    
-    
+    /* Initialize all configured peripherals (HAL, GPIO, I2C, etc.) */
+    // ...
+    // ...
+	   // ...
+	
+   
+	   // Setup eeprom memory device 1 parameters
     memory1.I2Cx = hi2c1;
     memory1.WP_GPIO = GPIOB;
     memory1.WP_Pin = GPIO_PIN_3;
-    memory1.MemoryCode = I2C_MEM_CODE_AT24C16;
+    memory1.MemoryCode = I2C_MEM_CODE_AT24C512;
     memory1.A0_PinState = 0;
-    memory1.A0_PinState = 0;
-    memory1.A0_PinState = 0;
+    memory1.A1_PinState = 0;
+    memory1.A2_PinState = 0;
     
-    I2C_Memory_Init(&memory1); 
-    status = I2C_Memory_SingleWrite(&memory1, 8, 'A', 10); 
-    status = I2C_Memory_BurstWrite(&memory1, 100, "Hello", 5, 50); 
-    status = I2C_Memory_SingleRead(&memory1, 8, &data, 10);
-    status = I2C_Memory_BurstRead(&memory1, 100, data_list, 5, 50);   
-       
+    I2C_Memory_Init(&memory1);                                          // Initialize eeprom memory device 1
+    status[0] = I2C_Memory_SingleWrite(&memory1, 8, 'A', 10);           // Write single byte 'A' at address 8 with timeout 10ms 
+    status[1] = I2C_Memory_BurstWrite(&memory1, 100, "Hello", 5, 50);   // Write "Hello" starting at address 100 with timeout 50ms
+    status[2] = I2C_Memory_SingleRead(&memory1, 8, &data, 10);          // Read single byte from address 8 with timeout 10ms
+    status[3] = I2C_Memory_BurstRead(&memory1, 100, data_list, 5, 50);  // Read 5 bytes starting from address 100 with timeout 50ms
+	
+	
+    // Setup eeprom memory device 2 parameters 
     memory2.I2Cx = hi2c1;
     memory2.WP_GPIO = GPIOB;
     memory2.WP_Pin = GPIO_PIN_4;
     memory2.MemoryCode = I2C_MEM_CODE_AT24C64;
     memory2.A0_PinState = 1;
-    memory2.A1_PinState = 1;
+    memory2.A1_PinState = 0;
     memory2.A2_PinState = 1;
     
-    I2C_Memory_Init(&memory2);
-    status = I2C_Memory_SingleWrite(&memory2, 37, 'B', 10); 
-    status = I2C_Memory_BurstWrite(&memory2, 88, "World", 5, 50); 
-    status = I2C_Memory_SingleRead(&memory2, 37, &data, 10);
-    status = I2C_Memory_BurstRead(&memory2, 88, data_list, 5, 50);
+    I2C_Memory_Init(&memory2);                                         // Initialize eeprom memory device 2
+    status[4] = I2C_Memory_SingleWrite(&memory2, 37, 'B', 10);         // Write single byte 'B' at address 37 with timeout 10ms
+    status[5] = I2C_Memory_BurstWrite(&memory2, 88, "World", 5, 50);   // Write "World" starting at address 88 with timeout 50ms
+    status[6] = I2C_Memory_SingleRead(&memory2, 37, &data, 10);        // Read single byte from address 37 with timeout 10ms
+    status[7] = I2C_Memory_BurstRead(&memory2, 88, data_list, 5, 50);  // Read 5 bytes starting from address 88 with timeout 50ms
         
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-    
+ 
     while (1)
     {
-               
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-        
+		       // Your application code here   
     }
-    
-  /* USER CODE END 3 */
 }
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
